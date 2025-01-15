4c3df675018e239bcb6bd564fb91c94c9b7c14ed
AneeqaWaheed <aneeqawaheed19@gmail.com>
2025-01-15 22:33:11 +0500

    SignUp Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,15 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-=======
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap/dist/js/bootstrap.js";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUpForm from "./pages/Signup";
->>>>>>> Stashed changes
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<<<<<<< Updated upstream
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-=======
       {/* Don't delete code within this two comment */}
       {/* The code below is for the prompt and alert */}
       <div id="modalContainer"></div>
@@ -54,9 +25,8 @@ function App() {
           <Route path="/signup" element={<SignUpForm />} />
         </Routes>
       </BrowserRouter>
->>>>>>> Stashed changes
     </>
-  )
+  );
 }
 
-export default App
+export default App;
